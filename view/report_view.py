a93a2d8eae668a5c84a838a3ee32f38fa28f5863
Quentin Tellier <quentin.tellier@edu.ece.fr>
2025-10-21 16:54:30 +0200

    Modification du sous menu 3 rapports pour générer les rapports.

--- view/report_view.py
+++ view/report_view.py
@@ -16,10 +16,8 @@ class ReportView:
 
     def display_report_menu_view(self):
         table = Table(title="Menu Rapports", show_header=False, box=None)
-        table.add_row("[bold cyan]1.[/bold cyan] Afficher les joueurs")
-        table.add_row("[bold cyan]2.[/bold cyan] Afficher les tournois")
-        table.add_row("[bold cyan]3.[/bold cyan] Afficher les détails d'un tournoi")
-        table.add_row("[bold cyan]4.[/bold cyan] Retour")
+        table.add_row("[bold cyan]1.[/bold cyan] Générer les rapports")
+        table.add_row("[bold cyan]2.[/bold cyan] Retour")
         panel = Panel(table, title="[bold yellow]Gestion des Rapports[/bold yellow]", border_style="magenta")
         centered_panel = Align.center(panel)
         self.console.print(centered_panel)
@@ -37,6 +35,8 @@ class ReportView:
         with open(file_path, "w") as fh:
             fh.write(html_rendu)
 
+        self.display_successful_generation_message("joueurs")
+
     def display_tournaments_jinja_view(self, tournaments):
         env = Environment(loader=FileSystemLoader('templates'))
         template = env.get_template('tournaments.html.j2')
@@ -49,6 +49,8 @@ class ReportView:
         file_path = f'{directory}/tournaments.html'
         with open(file_path, "w") as fh:
             fh.write(html_rendu)
+
+        self.display_successful_generation_message("tournois")
     
     def display_tournament_players_jinja_view(self, tournament, players):
         env = Environment(loader=FileSystemLoader('templates'))
@@ -64,6 +66,8 @@ class ReportView:
         with open(file_path, "w") as fh:
             fh.write(html_rendu)
 
+        self.display_successful_generation_message(f"info joueurs tournoi {tournament_id}")
+
     def display_tournament_rounds_jinja_view(self, tournament, players):
         env = Environment(loader=FileSystemLoader('templates'))
         template = env.get_template('tournament_rounds.html.j2')
@@ -78,6 +82,9 @@ class ReportView:
         with open(file_path, "w") as fh:
             fh.write(html_rendu)
 
+        self.display_successful_generation_message(f"info tours tournoi {tournament_id}")
+        
+
     def display_index_jinja_view(self, tournaments, players):
         env = Environment(loader=FileSystemLoader('templates'))
         template = env.get_template('index.html.j2')
@@ -91,35 +98,38 @@ class ReportView:
         with open(file_path, "w") as fh:
             fh.write(html_rendu)
 
-    def display_link(self):
-        print("Visit my [link=https://www.willmcgugan.com]blog[/link]!")
+        self.display_successful_generation_message("accueil")
 
     def execute(self):
         running = True
         while running:
             self.display_report_menu_view()
-            choice = self.console.input("\n[bold green]Sélectionnez une option (1-4) : [/bold green]")
+            choice = self.console.input("\n[bold green]Sélectionnez une option (1-2) : [/bold green]")
             if choice == "1":
-                player_controller = ChessPlayerController()
-                players = player_controller.display_players_from_json()
-                self.display_players_jinja_view(players)
-            elif choice == "2":
-                tournament_controller = TournamentController()
-                tournaments = tournament_controller.display_tournaments()
-                self.display_tournaments_jinja_view(tournaments)
-            elif choice == "3":
                 player_controller = ChessPlayerController()
                 tournament_controller = TournamentController()
                 players = player_controller.display_players_from_json()
                 tournaments = tournament_controller.display_tournaments()
                 self.display_index_jinja_view(tournaments, players)
+                self.display_players_jinja_view(players)
+                self.display_tournaments_jinja_view(tournaments)
                 for tournament in tournaments:
                     self.display_tournament_players_jinja_view(tournament, players)
                     self.display_tournament_rounds_jinja_view(tournament, players)
-            elif choice == "4":
+                self.display_link()
+            elif choice == "2":
                 running = False
             else:
                 self.display_invalid_choice_message()
 
     def display_invalid_choice_message(self):
-        self.console.print(Align.center("[bold red]Choix invalide, veuillez réessayer.[/bold red]"))
+        self.console.print(Align.left("[bold red]Choix invalide, veuillez réessayer.[/bold red]"))
+
+    def display_successful_generation_message(self, file):
+        self.console.print(Align.left(f"[yellow]Le rapport {file} à été généré.[/yellow]"))
+
+    def display_link(self):
+        self.console.print(Align.left(f"[bold bright_magenta]------------------------[/bold bright_magenta]"))
+        self.console.print(Align.left(f"[bold bright_magenta][link=./reports/index.html]Consultez les rapports ![/link][/bold bright_magenta]"))
+        self.console.print(Align.left(f"[bold bright_magenta]------------------------[/bold bright_magenta]"))
+        self.console.print(Align.left("Si le lien ne fonctionne pas, dans le dossier Reports ouvrez index.html dans un navigateur. "))
